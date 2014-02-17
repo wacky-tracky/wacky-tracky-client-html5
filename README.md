b592cfe4d784b9f3bfda5f2f462e1f7199ff3d20
jamesread <jread@redhat.com>
2014-02-17 08:14:21 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-wacky-tracky
-============
+ohai
+
+test
---
+++ main.js
@@ -0,0 +1,306 @@
+window.host = "http://localhost:8082/"
+
+function Task(taskObject) {
+	var self = this;
+	this.parent = null;
+	this.fields = taskObject;
+
+	this.dom = $('<div class = "taskWrapper" />')
+	this.domTask = this.dom.createAppend('<div class = "task" />').text('Task: ' + this.fields.content);
+	this.domTask.dblclick(function() { self.setParentTask(); });
+	this.domTaskButtons = this.domTask.createAppend('<div class = "taskButtons" />');
+	this.domButtonDelete = this.domTaskButtons.createAppend('<button>delete</button>');
+	this.domButtonDelete.click(function() { self.del(); });
+
+	this.domSubtasks = this.dom.createAppend('<div class = "subTasks" />');
+
+	this.subtasks = [];
+
+	Task.prototype.addSubtask = function(t) {
+		t.parent = this;
+		this.domSubtasks.append(t.toDom());
+		this.subtasks.push(t);
+	};
+
+	Task.prototype.toDom = function() {
+		return this.dom;
+	};
+
+	Task.prototype.setParentTask = function() {
+		if (window.parentTask !== null) {
+			window.parentTask.deselect();
+		}
+
+		window.parentTask = this;
+		this.dom.addClass('selected');
+	};
+
+	Task.prototype.deselect = function() {
+		window.parentTask = null;
+		this.dom.removeClass('selected');
+	};
+
+	Task.prototype.del = function(i) {
+		$.ajax({
+			url: window.host + '/api/deleteTask',
+			data: { id: this.fields.id }
+		});
+
+		this.dom.remove();
+		window.content.list.tasks.pop(this);
+		window.content.list.updateTaskCount();
+	};
+
+	return this;
+}
+
+function init() {
+	window.parentTask = null;
+
+	window.sidebar = new Sidebar();
+	$('body').append(window.sidebar.toDom());
+
+	requestLists();
+
+	window.content = new Content();
+	$('body').append(window.content.toDom());
+
+	sidebarResized();
+}
+
+function Content() {
+	this.dom = $('<div id = "content" />');
+	this.taskInput = new TaskInputBox();
+	this.dom.append(this.taskInput.toDom());
+	this.domListContainer = this.dom.createAppend('<div>');
+
+	this.list = null;
+
+	Content.prototype.toDom = function() {
+		return this.dom;
+	};
+
+	Content.prototype.setList = function(list) {
+		window.parentTask = null;
+
+		this.list = list;
+		this.domListContainer.children().remove();
+		this.domListContainer.append(list.toDomContent());
+
+		this.taskInput.enable();
+	};
+
+	return this;
+}
+
+function newTask(text) {
+	$('input#task').val('');
+
+	data = { content: text };
+	
+	if (window.parentTask === null) {
+		data.parentId = window.content.list.fields.id;
+		data.parentType = 'list';
+	} else {
+		data.parentId = window.parentTask.fields.id;
+		data.parentType = 'task';
+	}
+
+	$.ajax({
+		url: window.host + '/api/createTask',
+		success: renderTaskCreated,
+		data: data
+	});
+}
+
+function renderTaskCreated(task) {
+	window.content.list.add(task);
+}
+
+function TaskInputBox(label) {
+	this.label = null;
+
+	this.dom = $('<div />');
+	this.domInput = this.dom.createAppend('<input id = "task" value = "" />');
+	this.domInput.attr('disabled', 'disabled');
+	this.domInput.model(this);
+	this.domInput.keypress(function(e) {
+		var key = e.keyCode ? e.keyCode : e.which;
+
+		if (key == 13) {
+			newTask($(this).val(), window.content.list.fields.id);
+		}
+	});
+
+	TaskInputBox.prototype.toDom = function() {
+		return this.dom;
+	};
+
+	TaskInputBox.prototype.enable = function() {
+		this.domInput.removeAttr('disabled');
+		this.domInput.focus();
+	};
+
+	return this;
+}
+
+$.fn.createAppend = function(constructor) {
+	var childElement = $(constructor);
+
+	$(this).append(childElement);
+
+	return $(childElement);
+};
+
+$.fn.model = function() {
+	if (typeof(this.data('model')) == "undefined") {
+		this.data('model', {});
+	}
+
+	return this.data('model');
+};
+
+function renderTasks(list) {
+	window.content.list.addAll(list);
+}
+
+function generalError(msg, errorText) {
+	console.log("error", msg, errorText);
+	$('body').createAppend($('<div class = "notification">').text('Error: ' + errorText)).click(function() {
+		this.remove();	
+	});
+}
+
+function requestTasks(list) {
+	window.content.setList(list);
+
+	$.ajax({
+	url: window.host + 'api/listTasks',
+		data: { list: list.fields.id },
+		success: renderTasks,
+		error: generalError
+	});
+}
+
+function List(jsonList) {
+	this.fields = jsonList;
+
+	this.domSidebar = $('<li class = "list" />');
+	this.domSidebar.model(this);
+	this.domSidebarTitle = this.domSidebar.createAppend('<a href = "#" class = "listTitle" />').text(this.fields.title);
+	this.domSidebarTitleSuffix = this.domSidebarTitle.createAppend('<span class = "subtle" />');
+
+	this.domList = $('<ul id = "taskList" />');
+
+	this.tasks = [];
+
+	var self = this;
+
+	this.updateTaskCount = function() {
+		this.domSidebarTitleSuffix.text(this.tasks.length);
+	};
+
+	this.domSidebarTitle.click(function(e) {
+		requestTasks(self);
+
+		window.sidebar.deselectAll();
+		self.domSidebar.addClass('selected');
+	});
+
+	List.prototype.toDomSidebar = function () {
+		return this.domSidebar;
+	}; 
+
+	List.prototype.toDomContent = function() {
+		return this.domList;
+	};
+
+	List.prototype.addAll = function(tasks) {
+		var self = this;
+
+		this.clear();
+
+		$(tasks).each(function(index, item) {	
+			self.add(item);
+		});
+		
+		self.updateTaskCount();
+	};
+
+	List.prototype.add = function(item) {
+		task = new Task(item);
+
+		this.tasks.push(task);
+		this.domList.append(task.toDom());
+
+		this.updateTaskCount();
+	};
+
+	List.prototype.deselect = function() {
+		this.domSidebar.removeClass('selected');
+	};
+
+	List.prototype.clear = function() {
+		this.domList.children().remove();
+		this.tasks.length = 0;
+	};
+
+	this.updateTaskCount();
+
+	return this;
+}
+
+function sidebarResized() {
+	window.content.dom.css('left', window.sidebar.dom.css('width'));
+	window.content.dom.css('right', $('body').css('width'));
+}
+
+function Sidebar() {
+	var self = this;
+
+	this.dom = $('<div id = "sidebar" />');
+	this.dom.model(this);
+	this.dom.resizable({ minWidth: 200, handles: 'e', resize: sidebarResized});
+	this.domTitle = this.dom.createAppend('<h2>WackyTracky</h2>');
+	this.domLists = this.dom.createAppend('<ul class = "lists" />');
+	this.domButtonNewList = this.dom.createAppend('<button>New...</button>').click(function() { self.createList(); });
+
+	this.lists = [];
+
+	Sidebar.prototype.createList = function() {
+		var title = window.prompt("List name?");
+
+		$.ajax({
+			url: window.host + 'api/createList',
+			data: {
+				title: title
+			}
+		});
+
+		requestLists();
+	}
+
+	Sidebar.prototype.addList = function(list) {
+		this.domLists.append(list.toDomSidebar());
+		this.lists.push(list);
+	};
+
+	Sidebar.prototype.toDom = function() {
+		return this.dom;
+	};
+
+	Sidebar.prototype.clear = function() {
+		this.domLists.children().remove();
+		this.lists.length = 0;
+	}
+
+	Sidebar.prototype.deselectAll = function() {
+		$(this.lists).each(function(index, list) {
+			list.deselect();
+		});
+	};
+
+	return this;
+}
+
+
