4ce5abd4e3e7d34bd472c752dd698922a883e28d
jamesread <contact@jwread.com>
2015-03-02 18:07:41 +0000

    Added support for opening sublists with right, closing them with left, and toggling them with space.

--- main.js
+++ main.js
@@ -99,14 +99,19 @@ function Task(taskObject) {
 		return this.domSubtasks.css('display') == 'block';
 	};
 
-	Task.prototype.toggleSubtasks = function() {
-		if (this.isSubtasksVisible()) {
-			this.domSubtasks.css('display', 'none') ;
-		} else {
+	Task.prototype.setSubtasksVisible = function(visible) {
+		if (visible) {
+			this.refreshSubtasks();
 			this.domSubtasks.css('display', 'block') ;
+		} else {
+			this.domSubtasks.css('display', 'none') ;
 		}
 
 		this.refreshExpandButton();
+	};
+
+	Task.prototype.toggleSubtasks = function() {
+		this.setSubtasksVisible(!this.isSubtasksVisible());
 	};
 
 	Task.prototype.refreshExpandButton = function(forceEnabled) {
@@ -1160,27 +1165,51 @@ function SidePanel() {
 	return this;
 }
 
+KeyCodes = {}
+KeyCodes.ESC = 27;
+KeyCodes.DEL = 46;
+KeyCodes.F2 = 113;
+KeyCodes.UP = 38;
+KeyCodes.DOWN = 40;
+KeyCodes.RIGHT = 39;
+KeyCodes.LEFT = 37;
+KeyCodes.SPACE = 32;
+
 $(document).keyup(function(e) {
 	if (window.selectedItem !== null) {
-		if (e.keyCode == 27) {
+		if (e.keyCode == KeyCodes.ESC) {
 			window.selectedItem.deselect();
 		}
 
-		if (e.keyCode == 46) {
+		if (e.keyCode == KeyCodes.DEL) {
 			window.selectedItem.del();
 		}
 
-		if (e.keyCode == 113) {
+		if (e.keyCode == KeyCodes.F2) {
 				window.selectedItem.rename();
 		}
 
-		if (e.keyCode == 40) {
+		if (e.keyCode == KeyCodes.DOWN) {
 			window.content.list.nextSelect();
 		}
 
-		if (e.keyCode == 38) {
+		if (e.keyCode == KeyCodes.UP) {
 			window.content.list.prevSelect();
 		}
+
+		if (e.keyCode == KeyCodes.RIGHT) {
+			window.selectedItem.setSubtasksVisible(true);
+		}
+
+		if (e.keyCode == KeyCodes.LEFT) {
+			window.selectedItem.setSubtasksVisible(false);
+		}
+
+		if (e.keyCode == KeyCodes.SPACE) {
+			window.selectedItem.toggleSubtasks();
+		}
+
+		console.log(e.keyCode);
 	}
 
 	if (window.currentMenu != null) {
