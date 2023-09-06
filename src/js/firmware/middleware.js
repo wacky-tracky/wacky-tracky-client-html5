1ca4f8e42449b1bd0aba9c90054d77e2422f0b1c
jamesread <contact@jread.com>
2023-09-06 11:12:03 +0100

    Mostly formatting changes

--- src/js/firmware/middleware.js
+++ src/js/firmware/middleware.js
@@ -1,73 +1,73 @@
 import { generalError } from "./util.js"
 
 export function ajaxRequest(params) {
-	if (typeof(params.error) != "function") {
-		params.error = generalError
-	}
+  if (typeof(params.error) != "function") {
+    params.error = generalError
+  }
 
-	if (typeof(params.success) != "function") {
-		throw new Error("Success callback is not a function, it is: " + typeof(params.success));
-	}
+  if (typeof(params.success) != "function") {
+    throw new Error("Success callback is not a function, it is: " + typeof(params.success));
+  }
 
-	// Assigning the callbacks to variables here is important, because the fetch 
-	// promise mangles the "this" context on the callbacks. 
-	let callbackSuccess = (data) => { params.success(data) };
+  // Assigning the callbacks to variables here is important, because the fetch 
+  // promise mangles the "this" context on the callbacks. 
+  let callbackSuccess = (data) => { params.success(data) };
 
-	let callbackError = (data) => { params.error(data); }
+  let callbackError = (data) => { params.error(data); }
 
-	let isSuccessful = true;
+  let isSuccessful = true;
 
 
-	let url = '//' + window.location.host + "/api/" + params.url;
+  let url = '//' + window.location.hostname + ":8082/api/" + params.url;
 
-	if (typeof(params.data) !== "undefined") {
-		Object.keys(params.data).forEach(key => url.searchParams.append(key, params.data[key]))
-	}
+  if (typeof(params.data) !== "undefined") {
+    Object.keys(params.data).forEach(key => url.searchParams.append(key, params.data[key]))
+  }
 
-	var f1 = fetch(url, {
-		method: 'GET',
-		credentials: 'include',
-	})
-	.then(resp => {
-		if (!resp.ok) {
-      Promise.reject(resp)
+  var f1 = fetch(url, {
+    method: 'GET',
+    credentials: 'include',
+  })
+    .then(resp => {
+      if (!resp.ok) {
+        Promise.reject(resp)
 
-			isSuccessful = false;
-			return "Fetch request is not OK";
-		}
+        isSuccessful = false;
+        return "Fetch request is not OK";
+      }
 
-		return resp.json();
-	})
-	.then(json => {
-		if (isSuccessful) {
-			callbackSuccess(json)
-		} else {
-			callbackError(json);	
-		}
-	}).catch(err => {
-    Promise.reject(err)
-		callbackError(err)
-	});
+      return resp.json();
+    })
+    .then(json => {
+      if (isSuccessful) {
+        callbackSuccess(json)
+      } else {
+        callbackError(json);	
+      }
+    }).catch(err => {
+      Promise.reject(err)
+      callbackError(err)
+    });
 
 }
 
 // FIXME move to UiManager
 export function clearValidationFailures() {
-	document.querySelectorAll('p.validationError').forEach(e => e.remove());
-	document.querySelectorAll('input.validationError').forEach(e => e.classList -= 'validationError');
+  document.querySelectorAll('p.validationError').forEach(e => e.remove());
+  document.querySelectorAll('input.validationError').forEach(e => e.classList -= 'validationError');
 }
 
 // FIXME move to UiManager
 export function highlightValidationFailure(selector, message) {
-	let element = document.querySelector(selector)
+  let element = document.querySelector(selector)
 
-	element.classList += 'validationError'
+  element.classList += 'validationError'
 
-	let errorMessage = document.createElement("p")
-	errorMessage.classList += "validationError"
-	errorMessage.innerText = message
+  let errorMessage = document.createElement("p")
+  errorMessage.classList += "validationError"
+  errorMessage.innerText = message
 
-	element.parentElement.querySelectorAll('p.validationError').forEach(e => e.remove());
-	element.parentElement.appendChild(errorMessage);
+  element.parentElement.querySelectorAll('p.validationError').forEach(e => e.remove());
+  element.parentElement.appendChild(errorMessage);
 
 }
--- src/js/modules/Bootloader.js
+++ src/js/modules/Bootloader.js
@@ -11,43 +11,43 @@ render either a working UI, or sensible error messages.
 The bootloader hands off to the UiManager when complete.
 */
 export class Bootloader {
-	init() {
-		setBootMessage("Bootloader init");
+  init() {
+    setBootMessage("Bootloader init");
 
-		setBootMessage("Waiting for database");
-		window.dbal = new DBAL();
-		window.dbal.open(() => { this.initAfterDatabase() });
-	} 
+    setBootMessage("Waiting for database");
+    window.dbal = new DBAL();
+    window.dbal.open(() => { this.initAfterDatabase() });
+  } 
 
-	initAfterDatabase() {
-		setBootMessage("Database is OK");
-		try {
-		ajaxRequest.bind(this, {
-			url: "Init",
-			success: this.initSuccess.bind(this),
-			error: this.initFailure.bind(this)
-		}).call()
-		} catch (e) {
-			console.log("ex", e);
-		}
-	}
+  initAfterDatabase() {
+    setBootMessage("Database is OK");
+    try {
+      ajaxRequest.bind(this, {
+        url: "Init",
+        success: this.initSuccess.bind(this),
+        error: this.initFailure.bind(this)
+      }).call()
+    } catch (e) {
+      console.log("ex", e);
+    }
+  }
 
-	/**
-	At this stage, the critical "boot" path is complete. 
-	We have the minimum browser features, core javascript, etc.
-	*/
-	initSuccess(res) {
-		window.uimanager.onBootloaderSuccess(res);
-	}
+  /**
+  At this stage, the critical "boot" path is complete. 
+  We have the minimum browser features, core javascript, etc.
+  */
+  initSuccess(res) {
+    window.uimanager.onBootloaderSuccess(res);
+  }
 
-	initFailure(a, b, c) {
-		if (a != null && a.toString().includes("Failed to fetch")) {
-//`			setBootMessage("Failed to fetch during init, are you offline?");
-			window.uimanager.onBootloaderOffline();
-		} else {
-			console.log(a, b, c);
-			setBootMessage("Unknown init failure.");
-		}
+  initFailure(a, b, c) {
+    if (a != null && a.toString().includes("Failed to fetch")) {
+      //`     setBootMessage("Failed to fetch during init, are you offline?");
+      window.uimanager.onBootloaderOffline();
+    } else {
+      console.log(a, b, c);
+      setBootMessage("Unknown init failure.");
+    }
 
-	}	
+  } 
 }
