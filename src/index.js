e3aa3e11ada8e401f3021076a386f151a3dc4fb2
jamesread <contact@jread.com>
2024-01-21 20:31:42 +0000

    fmt

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import "./buildid.js";
+import './buildid.js'
 
 // "firmware" is essential non-modularized code.
-import "./js/firmware/middleware.js"
-import "./js/firmware/keyboardShortcuts.js";
-//import "./js/firmware/sw_loader.js";
+import './js/firmware/middleware.js'
+import './js/firmware/keyboardShortcuts.js'
+// import "./js/firmware/sw_loader.js"
 
-// webcomponents are modularized UI elements, that are needed by createElement, 
+// webcomponents are modularized UI elements, that are needed by createElement,
 // and cannot be imported as-needed so they are imported here. They all call
 // defineElement in their source files.
-import './js/modules/webcomponents/LoginForm.js';
-import './js/modules/webcomponents/TaskContent.js';
-import './js/modules/webcomponents/SidePanel.js';
-import './js/modules/webcomponents/SidePanelTagButton.js';
-import "./js/modules/webcomponents/ListContent.js";
-import "./js/modules/webcomponents/ContentPanel.js";
+import './js/modules/webcomponents/LoginForm.js'
+import './js/modules/webcomponents/TaskContent.js'
+import './js/modules/webcomponents/SidePanel.js'
+import './js/modules/webcomponents/SidePanelTagButton.js'
+import './js/modules/webcomponents/ListContent.js'
+import './js/modules/webcomponents/ContentPanel.js'
 
-import { UiManager } from "./js/modules/UiManager.js";
-window.uimanager = new UiManager();
+import { UiManager } from './js/modules/UiManager.js'
+import { Bootloader } from './js/modules/Bootloader.js'
 
-import { Bootloader } from "./js/modules/Bootloader.js";
-window.bootloader = new Bootloader();
-window.bootloader.init();
+window.uimanager = new UiManager()
+
+window.bootloader = new Bootloader()
+window.bootloader.init()
